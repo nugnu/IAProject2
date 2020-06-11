f087e610dcb65b6287a463e2b8941bba201c3d09
nugnu <gulibraiz@gmail.com>
2020-06-11 05:23:00 -0300

    adicionando torneio, metodos de mutacao, metodos de replacement. arrumando a logica para funcionar para o sudoku

--- genetic_problems.py
+++ genetic_problems.py
@@ -1,12 +1,13 @@
 from collections import deque
 from collections import Counter
 from utils import *
+import numpy as np
 
 class GeneticProblem:
    
-    def __init__(self, gene_pool=None, initial=None, f_threshold=None, initial_population_size=30, 
-    max_ngen=1000, mut_rate=0.2, crossover_rate=1, mut_type="swap", crossover_type="one_point"):
-        self.gene_pool = gene_pool
+    def __init__(self, initial=None, f_threshold=None, initial_population_size=30, 
+    max_ngen=1000, mut_rate=0.2, crossover_rate=1, mut_type="swap", crossover_type="one_point",
+    selection_type = "tournament", replacement_type = "default"):
         self.initial = initial
         self.f_threshold = f_threshold
         self.initial_population_size = initial_population_size
@@ -15,6 +16,8 @@ class GeneticProblem:
         self.crossover_rate = crossover_rate
         self.mut_type = mut_type
         self.crossover_type = crossover_type
+        self.selection_type =  selection_type
+        self.replacement_type = replacement_type
         self.population = []
 
     def fitness(self):
@@ -37,14 +40,30 @@ class GeneticProblem:
     
 class SudokuGeneticProblem(GeneticProblem):
 
-    def __init__(self, gene_pool=None, initial=None, f_threshold=None, initial_population_size=1000,
-    max_ngen=1000, mut_rate=0.2, crossover_rate=1, mut_type="swap", crossover_type="one_point"):
-        super().__init__(self, gene_pool, initial, f_threshold, initial_population_size, max_ngen, mut_rate,
-        crossover_rate, mut_type, crossover_type)
+    def __init__(self, initial, f_threshold=None, initial_population_size=1000,
+    max_ngen=1000, mut_rate=0.2, crossover_rate=1, mut_type="swap", crossover_type="one_point",
+    selection_type = "tournament", replacement_type = "default"):
+        super().__init__(self, initial, f_threshold, initial_population_size, max_ngen, mut_rate,
+        crossover_rate, mut_type, crossover_type, selection_type, replacement_type)
         self.__SUDOKU_ARRAY_SIZE = 81
         self.__MAX_FITNESS = 1000000 # best possible fitness value -> to be fixed 
+        self.__MAX_SWAP = 10 # max possible swaps to be done on a single mutation
+        self.__MAX_FLIP = 10 # max possible random gene change to be done on a single mutation
+        self.gene_pool = range(1,10)
         self.gen = 0 
         self.population = []
+        self.emptyArray = __initEmptyArray() # each position of this array keeps the position of an empty position on a sudoku puzzle 
+
+    def __initEmptyArray(self):
+        return np.nonzero(self.initial == 0) # returns the indices of the zero elements on the sudoku puzzle
+
+    def __tournament(self, k):
+        competitors = []
+        for _ in range(k):            
+            pop_len = len(self.population)
+            c = random.randrange(pop_len)
+            competitors.append(population[c])
+        return max(competitors, key=self.fitness)
 
     def fitness(self, individual): # TODO
         return 1
@@ -52,9 +71,14 @@ class SudokuGeneticProblem(GeneticProblem):
     def init_population(self):
         g = len(self.gene_pool)
         for i in range(self.pop_number):
-            new_individual = [self.gene_pool[random.randrange(0, g)] for j in range(self.__SUDOKU_ARRAY_SIZE)]
+            new_individual = []
+            for j in range(self.__SUDOKU_ARRAY_SIZE):
+                if (j not in self.emptyArray):
+                    new_individual[j] = self.initial[j]
+                else:
+                    new_individual[j] = self.gene_pool[random.randrange(0, g)]
             self.population.append(new_individual)
-
+            
     def goal_test(self, individual):
         if (self.f_threshold != None):
             if (self.max_ngen != None):
@@ -68,18 +92,23 @@ class SudokuGeneticProblem(GeneticProblem):
                 return (fitness(individual) == self.__MAX_FITNESS)
 
     def selection(self, r):
-        fitnesses = map(self.fitness, self.population)
-        sampler = weighted_sampler(self.population, fitnesses)
-        return [sampler() for i in range(r)]
+        if (self.selection_type == "roulette"):
+            fitnesses = map(self.fitness, self.population)
+            sampler = weighted_sampler(self.population, fitnesses)
+            return [sampler() for i in range(r)]
+        
+        if (self.selection_type == "tournament"):
+            k = 3 # number of tournament competitors
+            return [self.__tournament(k) for i in range(r)]
 
     def crossover(self, x, y): 
         if random.uniform(0, 1) >= self.crossover_rate:
             return x # returns a individual from the current generation without doing the crossover 
 
         if (self.crossover_type == "one_point"):
-            n = len(x)
-            c = random.randrange(0, n)
-            return x[:c] + y[c:]
+            c = random.randrange(0, len(self.emptyArray))
+            pos = c[self.emptyArray]
+            return x[:pos] + y[pos:]
 
         # TODO -> MORE crossover types 
         
@@ -89,25 +118,63 @@ class SudokuGeneticProblem(GeneticProblem):
         if random.uniform(0, 1) >= self.mut_rate:
             return x
 
-        if (self.mut_type == "swap"):
-            n = len(x)
+        if (self.mut_type == "flip"): # changes a random gene from the individual
             g = len(self.gene_pool)
-            c = random.randrange(0, n)
-            r = random.randrange(0, g)
-            new_gene = self.gene_pool[r]
-            return x[:c] + [new_gene] + x[c + 1:]
+            c = random.randrange(0, len(self.emptyArray)) # select a random position of the empty array
+            new_gene = self.gene_pool[random.randrange(0,g)]
+            x[self.emptyArray[c]] = new_gene
+            return x
+
+        if (self.mut_type == "nflip"): # perform n (1 up to __MAX_FLIP) flips    
+            n = random.randrange(1, self.__MAX_FLIP)
+            for _ in range(n):
+                g = len(self.gene_pool)
+                c = random.randrange(0, len(self.emptyArray)) # select a random position of the empty array
+                new_gene = self.gene_pool[random.randrange(0,g)]
+                x[self.emptyArray[c]] = new_gene
+            return x
+
+        if (self.mut_type == "swap"): # swap two genes from the individual
+            empty_array_len = len(self.emptyArray)
+            c1 = random.randrange(0, empty_array_len) # select a random position of the empty array
+            c2 = random.randrange(0, empty_array_len) # select another random position of the empty array
+            tmp = x[self.emptyArray[c1]]
+            x[self.emptyArray[c1]] = x[self.emptyArray[c2]]
+            x[self.emptyArray[c2]] = tmp
+            return x
+
+        if (self.mut_type == "nswap"): # performs swap n (1 up to __MAX_SWAP) times 
+            empty_array_len = len(self.emptyArray)
+            n = random.randrange(1, self.__MAX_SWAP)
+            for _ in range(n):
+                c1 = random.randrange(0, empty_array_len) # select a random position of the empty array
+                c2 = random.randrange(0, empty_array_len) # select another random position of the empty array
+                tmp = x[self.emptyArray[c1]]
+                x[self.emptyArray[c1]] = x[self.emptyArray[c2]]
+                x[self.emptyArray[c2]] = tmp
+            return x
 
         # TODO -> MORE mutation types 
 
         return x
 
     def replace_population(self, prev_fitness_individual=None):
-        # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
-        # do it len(population) times 
-        self.population = [ mutate(crossover(*selection(2))) for i in range (len(self.population)) ] 
-    
-        # if needed, add the fittest of previous generation
-        if (prev_fitness_individual != None):
-            self.population.append(prev_fitness_individual)
+        if (self.replacement_type == "default"):
+            # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
+            # do it len(population) times 
+            self.population = [ mutate(crossover(*selection(2))) for i in range (len(self.population)) ] 
+        
+            # if needed, add the fittest of previous generation
+            if (prev_fitness_individual != None):
+                self.population.append(prev_fitness_individual)
+
+        if (self.replacement_type == "elitism"): # similar to default but the population doesnt get bigger (same size as initial population)
+            # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
+            # do it len(population) times 
+            self.population = [ mutate(crossover(*selection(2))) for i in range (len(self.population) - 1 ) ] 
+        
+            # if needed, add the fittest of previous generation
+            if (prev_fitness_individual != None):
+                self.population.append(prev_fitness_individual)
 
     
