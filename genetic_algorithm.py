6ef42a32a5bf342aaed53f08a27363a12cd9f64d
nugnu <gulibraiz@gmail.com>
2020-06-13 05:35:57 -0300

    able to use all methods

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -8,8 +8,8 @@ from genetic_problems import *
 # Genetic Algorithm
 
 def genetic_algorithm(problem):
-    MAX_STALE = 200
-    KILL_PERCENT = 0.98
+    MAX_STALE = 100
+    DELTA = 5 # measure how far a population is far from the solution
     problem.init_population()
     stale = 0
     while True:
@@ -18,7 +18,7 @@ def genetic_algorithm(problem):
             print("GEN " + str(problem.gen) + " SOLUTION FOUND: \n" + problem.print_individual(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
             return prev_fitness_individual
         # print the best current individual
-        print("GEN " + str(problem.gen) + " BEST INDIVIDUAL: \n" + problem.print_individual(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
+        print("GEN "    + str(problem.gen) + " BEST INDIVIDUAL: \n" + problem.print_individual(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
         problem.replace_population(prev_fitness_individual) # replace the population and add the fittest of the previous generation to the new one 
         current_fitness_individual = max(problem.population, key = problem.fitness)
         
@@ -27,7 +27,10 @@ def genetic_algorithm(problem):
         else: stale = 0
 
         if (stale > MAX_STALE): 
+            KILL_PERCENT = 1
+            if (problem.fitness(current_fitness_individual) < problem.MAX_FITNESS - DELTA):
+                KILL_PERCENT = 1
             print("RE-SEEDING POPULATION\n")
             problem.stale_handler(KILL_PERCENT)
             stale = 0
-        problem.gen = problem.gen + 1
+        problem.gen += 1
--- genetic_problems.py
+++ genetic_problems.py
@@ -2,6 +2,7 @@ from collections import deque
 from collections import Counter
 from utils import *
 import numpy as np
+import copy 
 
 class GeneticProblem:
    
@@ -46,20 +47,19 @@ class SudokuGeneticProblem(GeneticProblem):
 
     # GENERAL
 
-    def __init__(self, initial, f_threshold=None, initial_population_size=1100,
-    max_ngen=10000, mut_rate=0.2, crossover_rate=1, mut_type="swap", crossover_type="one_point",
-    selection_type = "tournament", replacement_type = "default", init_type = "smart"):
+    def __init__(self, initial, f_threshold=None, initial_population_size=1200,
+    max_ngen=10000, mut_rate=0.35, crossover_rate=1, mut_type="swap", crossover_type="one_point",
+    selection_type = "tournament", replacement_type = "elitism", init_type = "permutation"):
         super().__init__(initial, f_threshold, initial_population_size, max_ngen, mut_rate,
         crossover_rate, mut_type, crossover_type, selection_type, replacement_type, init_type)
         self.__SUDOKU_ARRAY_SIZE = 81
-        self.__MAX_FITNESS = 243 # best possible fitness value 
-        self.__MAX_SWAP = 5 # max possible swaps to be done on a single mutation
-        self.__MAX_FLIP = 5 # max possible random gene change to be done on a single mutation
+        self.__MAX_SWAP = 3 # max possible swaps to be done on a single mutation
+        self.__MAX_FLIP = 3  # max possible random gene change to be done on a single mutation
+        self.MAX_FITNESS = 243 # best possible fitness value 
         self.gene_pool = range(1,10)
         self.gen = 0 
         self.population = []
         self.emptyArray = self.__initEmptyArray() # each position of this array keeps the position of an empty position on a sudoku puzzle 
-        self.epsilon = 0.1
 
     def __initEmptyArray(self):
         return list(np.nonzero(self.initial == 0)[0]) # returns the indices of the zero elements on the sudoku puzzle
@@ -74,9 +74,9 @@ class SudokuGeneticProblem(GeneticProblem):
                 return (self.fitness(individual) >= self.f_threshold)
         else: # no threshold -> its expecting an actual solution (best possible fitness)
             if (self.max_ngen != None):
-                return (self.gen == self.max_ngen or self.fitness(individual) == self.__MAX_FITNESS)
+                return (self.gen == self.max_ngen or self.fitness(individual) == self.MAX_FITNESS)
             else:
-                return (self.fitness(individual) == self.__MAX_FITNESS)
+                return (self.fitness(individual) == self.MAX_FITNESS)
 
     # FITNESS
 
@@ -106,42 +106,56 @@ class SudokuGeneticProblem(GeneticProblem):
         )
 
     # INIT POPULATION
+
+    def __permutation_init(self):
+        # every solution is a permutation of (1,2,3,4,5,6,7,8,9,1,2,3...)
+        permutation = []
+        for _ in range(9):
+            permutation += np.arange(1,10).tolist()
+        # as we only want to solve the sudoku using the empty (0) spaces, we remove ocurrences of
+        # non zero initial values from the permutation
+        for number in self.initial[self.initial != 0]:
+            permutation.remove(number)
+        # now for each individual, shuffle this permutation and fill the empty spaces with it
+        for i in range(self.initial_population_size):
+            random.shuffle(permutation)
+            permut_iterator = 0
+            new_individual = []
+            for j in range(self.__SUDOKU_ARRAY_SIZE):
+                if (j not in self.emptyArray):
+                    new_individual.append(self.initial[j])  
+                else:
+                    new_individual.append(permutation[permut_iterator])
+                    permut_iterator += 1
+            self.population.append(new_individual)
+
+    def __random_init(self):
+        g = len(self.gene_pool)
+        for i in range(self.initial_population_size):
+            new_individual = []
+            for j in range(self.__SUDOKU_ARRAY_SIZE):
+                if (j not in self.emptyArray):
+                    new_individual.append(self.initial[j])  
+                else:
+                    new_individual.append(self.gene_pool[random.randrange(0, g)]) 
+            self.population.append(new_individual)
     
     def init_population(self):
-        if (self.init_type == "smart"): # avoid placing numbers more or less than 9 times (may lead to premature convergence!)  
-            # every solution is a permutation of (1,2,3,4,5,6,7,8,9,1,2,3...)
-            permutation = []
-            for _ in range(9):
-                permutation += np.arange(1,10).tolist()
-            # as we only want to solve the sudoku using the empty (0) spaces, we remove ocurrences of
-            # non zero initial values from the permutation
-            self.print_individual(self.initial)
-            for number in self.initial[self.initial != 0]:
-                permutation.remove(number)
-            print(permutation)
-            # now for each individual, shuffle this permutation and fill the empty spaces with it
-            for i in range(self.initial_population_size):
-                random.shuffle(permutation)
-                permut_iterator = 0
-                new_individual = []
-                for j in range(self.__SUDOKU_ARRAY_SIZE):
-                    if (j not in self.emptyArray):
-                        new_individual.append(self.initial[j])  
-                    else:
-                        new_individual.append(permutation[permut_iterator])
-                        permut_iterator += 1
-                self.population.append(new_individual)
+        m = 2 # number of methods
+        function_dict = {
+            0: self.__random_init,
+            1: self.__permutation_init
+        }
+
+        if (self.init_type == "all"):
+            c = random.randrange(0, m)
+            function_dict[c]()
+
+        if (self.init_type == "permutation"): # avoid placing numbers more or less than 9 times (may lead to premature convergence!)  
+            self.__permutation_init()
         
         if (self.init_type == "random"):
-            g = len(self.gene_pool)
-            for i in range(self.initial_population_size):
-                new_individual = []
-                for j in range(self.__SUDOKU_ARRAY_SIZE):
-                    if (j not in self.emptyArray):
-                        new_individual.append(self.initial[j])  
-                    else:
-                        new_individual.append(self.gene_pool[random.randrange(0, g)]) 
-                self.population.append(new_individual)
+            self.__random_init()
             
     # SELECTION
     
@@ -165,60 +179,96 @@ class SudokuGeneticProblem(GeneticProblem):
 
     # RECOMBINATION
 
+    def __one_point_crossover(self, x, y):
+        c = random.randrange(0, len(self.emptyArray))
+        pos = self.emptyArray[c]
+        return x[:pos] + y[pos:]
+
     def crossover(self, x, y): 
+        m = 1 # number of methods
+        function_dict = {
+            0: self.__one_point_crossover
+        }
+
         if random.uniform(0, 1) >= self.crossover_rate:
             return x # returns a individual from the current generation without doing the crossover 
 
-        if (self.crossover_type == "one_point"):
-            c = random.randrange(0, len(self.emptyArray))
-            pos = self.emptyArray[c]
-            return x[:pos] + y[pos:]
+        if (self.crossover_type == "all"):
+            c = random.randrange(0, m)
+            return function_dict[c](x, y)
 
+        if (self.crossover_type == "one_point"):
+            return self.__one_point_crossover(x, y)
+            
         # TODO -> MORE crossover types 
         
         return x
 
     # MUTATION
 
-    def mutate(self, x):
-        if random.uniform(0, 1) >= self.mut_rate:
-            return x
-
-        if (self.mut_type == "flip"): # changes a random gene from the individual
+    def __flip_mutate(self, x):
+        g = len(self.gene_pool)
+        c = random.randrange(0, len(self.emptyArray)) # select a random position of the empty array
+        new_gene = self.gene_pool[random.randrange(0,g)]
+        x[self.emptyArray[c]] = new_gene
+        return x
+    
+    def __nflip_mutate(self, x):
+        n = random.randrange(1, self.__MAX_FLIP)
+        for _ in range(n):
             g = len(self.gene_pool)
             c = random.randrange(0, len(self.emptyArray)) # select a random position of the empty array
             new_gene = self.gene_pool[random.randrange(0,g)]
             x[self.emptyArray[c]] = new_gene
-            return x
+        return x
 
-        if (self.mut_type == "nflip"): # perform n (1 up to __MAX_FLIP) flips    
-            n = random.randrange(1, self.__MAX_FLIP)
-            for _ in range(n):
-                g = len(self.gene_pool)
-                c = random.randrange(0, len(self.emptyArray)) # select a random position of the empty array
-                new_gene = self.gene_pool[random.randrange(0,g)]
-                x[self.emptyArray[c]] = new_gene
-            return x
+    def __swap_mutate(self, x):
+        empty_array_len = len(self.emptyArray)
+        c1 = random.randrange(0, empty_array_len) # select a random position of the empty array
+        c2 = random.randrange(0, empty_array_len) # select another random position of the empty array
+        tmp = x[self.emptyArray[c1]]
+        x[self.emptyArray[c1]] = x[self.emptyArray[c2]]
+        x[self.emptyArray[c2]] = tmp
+        return x
 
-        if (self.mut_type == "swap"): # swap two genes from the individual
-            empty_array_len = len(self.emptyArray)
+    def __nswap_mutate(self, x):
+        empty_array_len = len(self.emptyArray)
+        n = random.randrange(1, self.__MAX_SWAP)
+        for _ in range(n):
             c1 = random.randrange(0, empty_array_len) # select a random position of the empty array
             c2 = random.randrange(0, empty_array_len) # select another random position of the empty array
             tmp = x[self.emptyArray[c1]]
             x[self.emptyArray[c1]] = x[self.emptyArray[c2]]
             x[self.emptyArray[c2]] = tmp
+        return x
+
+    def mutate(self, x):
+        m = 4 # number of methods
+        function_dict = {
+            0: self.__flip_mutate,
+            1: self.__nflip_mutate,
+            2: self.__swap_mutate,
+            3: self.__nswap_mutate
+        }
+
+        if  random.uniform(0, 1) >= self.mut_rate:
             return x
 
+        if (self.mut_type == "all"):
+            c = random.randrange(0, m)
+            return function_dict[c](x)
+
+        if (self.mut_type == "flip"): # changes a random gene from the individual
+            return self.__flip_mutate(x)
+
+        if (self.mut_type == "nflip"): # perform n (1 up to __MAX_FLIP) flips    
+            return self.__nflip_mutate(x)
+
+        if (self.mut_type == "swap"): # swap two genes from the individual
+            return self.__swap_mutate(x)
+
         if (self.mut_type == "nswap"): # performs swap n (1 up to __MAX_SWAP) times 
-            empty_array_len = len(self.emptyArray)
-            n = random.randrange(1, self.__MAX_SWAP)
-            for _ in range(n):
-                c1 = random.randrange(0, empty_array_len) # select a random position of the empty array
-                c2 = random.randrange(0, empty_array_len) # select another random position of the empty array
-                tmp = x[self.emptyArray[c1]]
-                x[self.emptyArray[c1]] = x[self.emptyArray[c2]]
-                x[self.emptyArray[c2]] = tmp
-            return x
+            return self.__nswap_mutate(x)
 
         # TODO -> MORE mutation types 
 
@@ -226,25 +276,42 @@ class SudokuGeneticProblem(GeneticProblem):
 
     # REPLACE POPULATION
 
+    def __default_replace(self, prev_fitness_individual):
+        # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
+        # do it len(population) times 
+        self.population = [ self.mutate(self.crossover(*self.selection(2))) for i in range (len(self.population)) ] 
+    
+        # if needed, add the fittest of previous generation
+        if (prev_fitness_individual != None):
+            self.population.append(prev_fitness_individual)
+
+    def __elitism_replace(self, prev_fitness_individual):  # similar to default but the population doesnt get bigger (same size as initial population)
+        # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
+        # do it len(population) times 
+        self.population = [ self.mutate(self.crossover(*self.selection(2))) for i in range (len(self.population) - 1 ) ] 
+    
+        # if needed, add the fittest of previous generation
+        if (prev_fitness_individual != None):
+            self.population.append(prev_fitness_individual)
+
+
     def replace_population(self, prev_fitness_individual=None):
+        m = 2 # number of methods
+        function_dict = {
+            0: self.__default_replace,
+            1: self.__elitism_replace
+        }
+
+        if (self.replacement_type == "all"):
+            c = random.randrange(0, m)
+            function_dict[c](prev_fitness_individual)
+
         if (self.replacement_type == "default"):
-            # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
-            # do it len(population) times 
-            self.population = [ self.mutate(self.crossover(*self.selection(2))) for i in range (len(self.population)) ] 
-        
-            # if needed, add the fittest of previous generation
-            if (prev_fitness_individual != None):
-                self.population.append(prev_fitness_individual)
+            self.__default_replace(prev_fitness_individual)
 
-        if (self.replacement_type == "elitism"): # similar to default but the population doesnt get bigger (same size as initial population)
-            # apply crossover to 2 random individuals (more chance to the fittest) and them apply the mutation
-            # do it len(population) times 
-            self.population = [ self.mutate(self.crossover(*self.selection(2))) for i in range (len(self.population) - 1 ) ] 
-        
-            # if needed, add the fittest of previous generation
-            if (prev_fitness_individual != None):
-                self.population.append(prev_fitness_individual)
-
+        if (self.replacement_type == "elitism"):
+            self.__elitism_replace(prev_fitness_individual)
+           
     # STALE
 
     def kill_population(self, percent):
