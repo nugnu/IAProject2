f361f3ca34d0145bdb64965bcd33f30f3405bf5a
nugnu <gulibraiz@gmail.com>
2020-06-13 22:56:14 -0300

    to_string

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -16,10 +16,10 @@ def genetic_algorithm(problem):
     while True:
         prev_fitness_individual = max(problem.population, key=problem.fitness)
         if (problem.goal_test(prev_fitness_individual)): 
-            print("GEN " + str(problem.gen) + " SOLUTION FOUND: \n" + problem.print_individual(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
+            print("GEN " + str(problem.gen) + " SOLUTION FOUND: \n" + problem.to_string(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
             return prev_fitness_individual
         # print the best current individual
-        print("GEN "    + str(problem.gen) + " BEST INDIVIDUAL: \n" + problem.print_individual(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
+        print("GEN "    + str(problem.gen) + " BEST INDIVIDUAL: \n" + problem.to_string(prev_fitness_individual) + "\n", "FITNESS = " + str(problem.fitness(prev_fitness_individual)) + "\n")
         problem.replace_population(prev_fitness_individual) # replace the population and add the fittest of the previous generation to the new one 
         current_fitness_individual = max(problem.population, key = problem.fitness)
         
